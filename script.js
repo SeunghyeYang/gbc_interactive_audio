9dfbd1f45e99a06cb354087fbf7d1a4145aae083
SeunghyeYang <71297657+SeunghyeYang@users.noreply.github.com>
2020-12-14 18:43:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+console.clear();
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+
+const audioElement = document.querySelector('audio');
+const track = audioCtx.createMediaElementSource(audioElement);
+
+const playButton = document.querySelector('.happysong');
+const sadsongplayButton = document.querySelector('.sadsong');
+
+
+
+playButton.addEventListener('click', function() {
+	
+	if (audioCtx.state === 'suspended') {
+		audioCtx.resume();
+	}
+	
+	if (this.dataset.playing === 'false') {
+		audioElement.play();
+		this.dataset.playing = 'true';
+
+	} else if (this.dataset.playing === 'true') {
+		audioElement.pause();
+		this.dataset.playing = 'false';
+	}
+	
+	let state = this.getAttribute('aria-checked') === "true" ? true : false;
+	this.setAttribute( 'aria-checked', state ? "false" : "true" );
+	
+}, false);
+
+audioElement.addEventListener('ended', () => {
+	playButton.dataset.playing = 'false';
+	playButton.setAttribute( "aria-checked", "false" );
+}, false);
+
+sadsongplayButton.addEventListener('click', function() {
+	
+	if (audioCtx.state === 'suspended') {
+		audioCtx.resume();
+	}
+	
+	if (this.dataset.playing === 'false') {
+		audioElement.play();
+		this.dataset.playing = 'true';
+
+	} else if (this.dataset.playing === 'true') {
+		audioElement.pause();
+		this.dataset.playing = 'false';
+	}
+	
+	let state = this.getAttribute('aria-checked') === "true" ? true : false;
+	this.setAttribute( 'aria-checked', state ? "false" : "true" );
+	
+}, false);
+
+audioElement.addEventListener('ended', () => {
+	sadsongplayButton.dataset.playing = 'false';
+	sadsongplayButton.setAttribute( "aria-checked", "false" );
+}, false);
+
+const gainNode = audioCtx.createGain();
+
+const volumeControl = document.querySelector('[data-action="volume"]');
+volumeControl.addEventListener('input', function() {
+	gainNode.gain.value = this.value;
+}, false);
+
+const pannerOptions = {pan: 0};
+const panner = new StereoPannerNode(audioCtx, pannerOptions);
+
+const pannerControl = document.querySelector('[data-action="panner"]');
+pannerControl.addEventListener('input', function() {
+	panner.pan.value = this.value;	
+}, false);
+
+track.connect(gainNode).connect(panner).connect(audioCtx.destination);
+
+const powerButton = document.querySelector('.control-power');
+
+powerButton.addEventListener('click', function() {
+	if (this.dataset.power === 'on') {
+		audioCtx.suspend();
+		this.dataset.power = 'off';
+	} else if (this.dataset.power === 'off') {
+		audioCtx.resume();
+		this.dataset.power = 'on';
+	}
+	this.setAttribute( "aria-checked", state ? "false" : "true" );
+	console.log(audioCtx.state);
+}, false);
